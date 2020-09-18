554545be32f2e0377c9f51543e0f48fa169a88eb
user800-web <68912287+user800-web@users.noreply.github.com>
2020-09-18 11:33:46 -0500

    Crear matriz aletoria

--- Conversion.py
+++ Conversion.py
@@ -1,28 +1,3 @@
 import numpy as np
-num = float(input("Ingrese un numero: "))
-entero = int(num)
-decimal = num - entero
-res = []
-bin_ent = []
-while entero > 0:
-  bin_ent.append(str(entero % 2))
-  entero = entero// 2
-print("".join(bin_ent[::-1]))
-if decimal == 0.0:
-    res.append(0)
-else:
-    while decimal >= 0.01:
-        decimal = decimal * 2
-        if decimal >= 0 and decimal <= 0.999:
-            bin = 0
-            res.append(str(bin))
-        else:
-            bin_2 = 1
-            res.append(str(bin_2))
-        decimal = decimal - int(decimal)
-    if decimal >= 0.0009:
-        bin_3 = 0
-        res.append(str(bin_3))
-print("El decimal sobrante es:", decimal)
-res = ''.join(res)
-print("Decimal a binario :p","".join(bin_ent[::-1]), ".", res)
+A = np.random.randint(11, size = (10, 10))
+print(A)

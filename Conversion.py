05149c6742fbd2a352e9556d3655617f4de67d4f
user800-web <68912287+user800-web@users.noreply.github.com>
2020-09-18 11:43:13 -0500

    filas y columnas

--- Conversion.py
+++ Conversion.py
@@ -1,3 +1,10 @@
 import numpy as np
 A = np.random.randint(11, size = (10, 10))
 print(A)
+filas = len(A)
+columnas = len(A[0])
+R = []
+r = []
+for i in range(0, len(A)):
+    for j in range(0, len(A)):
+

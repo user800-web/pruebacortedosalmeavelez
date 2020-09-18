dce323b221652baccd7da60f03417e36510b07be
user800-web <68912287+user800-web@users.noreply.github.com>
2020-09-18 12:19:40 -0500

    Conteo elemento a elemento

--- Conversion.py
+++ Conversion.py
@@ -6,5 +6,20 @@ columnas = len(A[0])
 R = []
 r = []
 for i in range(0, len(A)):
-    for j in range(0, len(A)):
+  acum = 0
+  for j in range(0, len(A)):
+    if A[i][j] == 1:
+      r.append(A[i][j])
+    cont = len(r)
+R.append(cont)
+print(R)
+k = []
+for i in range(0, len(A)):
+  acum = 0
+  for j in range(0, len(A)):
+    if A[i][j] == 2:
+      k.append(A[i][j])
+    cont_dos = len(k)
+R.append(cont_dos)
+print(R)
 
